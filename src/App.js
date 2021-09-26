fdf0b9ce33b87af448befd39ac798ab988021780
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-09-26 21:59:46 +0530

    graph portion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <SortingVisualizer></SortingVisualizer>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={SortingVisualizer} />
+        <Route exact path='/graph' component={PathfindingVisualizer} />
+
+      </Switch>
+
+    </Router>
+
   );
 }
 
